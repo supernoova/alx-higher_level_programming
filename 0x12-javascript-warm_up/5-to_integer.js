db2c0b96ec09f26eceb4066ab18196334c370e28
supernoova <abdellatiffitah@gmail.com>
2024-06-19 01:40:05 +0100

    fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const argv = process.argv;
-
-if (isNaN(parseInt(argv[2])) === false) {
-  const num = Number(argv[2]);
-  console.log('My number: ' + num);
-} else { console.log('Not a number'); }

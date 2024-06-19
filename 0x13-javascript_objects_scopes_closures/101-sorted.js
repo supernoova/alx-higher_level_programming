b2e512646f2716fdfb64b791bda4c757e20bf195
supernoova <abdellatiffitah@gmail.com>
2024-06-19 15:21:10 -0700

    fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
-let newDict = {};
-let k;
-for (k in dict) {
-  newDict[dict[k]] = [];
+const dictInput = require('./101-data').dict;
+const outDict = {};
+
+for (const key in dictInput) {
+  const ocurr = dictInput[key];
+  outDict[ocurr] = [];
+  for (const keys in dictInput) {
+    if (dictInput[keys] === ocurr) {
+      outDict[ocurr].push(keys);
+    }
+  }
 }
-for (k in dict) {
-  newDict[dict[k]].push(k);
-}
-function cmp (a, b) {
-  return a - b;
-}
-for (k in newDict) {
-  newDict[k].sort(cmp);
-}
-console.log(newDict);
+console.log(outDict);
